6e41d8ab2f21039f2a44d2cfc103861d6821e303
LoveSy <shana@zju.edu.cn>
2021-02-26 03:27:59 +0800

    Update license

--- dex_builder.h
+++ dex_builder.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2018 The Android Open Source Project
+ * Modifications copyright (C) 2021 LSPosed Developers
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
