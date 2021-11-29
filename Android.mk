b5b72626d746c12b25b6723d2a8292ad76bff33d
kotori2 <admin@0x.mk>
2021-11-30 03:13:10 +0800

    Add reader
    Copied from https://android.googlesource.com/platform/tools/dexter, commit 4334b48

--- Android.mk
+++ Android.mk
@@ -5,7 +5,7 @@ LOCAL_MODULE            := dex_builder
 LOCAL_CPPFLAGS          := -std=c++17
 LOCAL_C_INCLUDES        := $(LOCAL_PATH)/slicer
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
-LOCAL_SRC_FILES         := dex_builder.cc slicer/writer.cc slicer/dex_ir.cc slicer/common.cc \
+LOCAL_SRC_FILES         := dex_builder.cc slicer/writer.cc slicer/reader.cc slicer/dex_ir.cc slicer/common.cc \
                            slicer/dex_format.cc slicer/dex_utf8.cc slicer/dex_bytecode.cc
 LOCAL_STATIC_LIBRARIES  := libcxx
 LOCAL_EXPORT_LDLIBS     := -lz
