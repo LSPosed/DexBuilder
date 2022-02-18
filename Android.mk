0e31176734f662f15d3c2abe5afd467abd060feb
LoveSy <shana@zju.edu.cn>
2022-02-19 02:11:58 +0800

    Update build script

--- Android.mk
+++ Android.mk
@@ -2,11 +2,22 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := dex_builder
-LOCAL_CPPFLAGS          := -std=c++17
+LOCAL_CPPFLAGS          := -std=c++20
 LOCAL_C_INCLUDES        := $(LOCAL_PATH)/slicer
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
 LOCAL_SRC_FILES         := dex_builder.cc slicer/writer.cc slicer/reader.cc slicer/dex_ir.cc slicer/common.cc \
                            slicer/dex_format.cc slicer/dex_utf8.cc slicer/dex_bytecode.cc
-LOCAL_STATIC_LIBRARIES  := libcxx
+LOCAL_EXPORT_LDLIBS     := -lz
+LOCAL_LDLIBS            := -lz
+LOCAL_CFLAGS            := -fvisibility=default -fvisibility-inlines-hidden
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := dex_builder_static
+LOCAL_CPPFLAGS          := -std=c++20
+LOCAL_C_INCLUDES        := $(LOCAL_PATH)/slicer
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
+LOCAL_SRC_FILES         := dex_builder.cc slicer/writer.cc slicer/reader.cc slicer/dex_ir.cc slicer/common.cc \
+                           slicer/dex_format.cc slicer/dex_utf8.cc slicer/dex_bytecode.cc
 LOCAL_EXPORT_LDLIBS     := -lz
 include $(BUILD_STATIC_LIBRARY)
